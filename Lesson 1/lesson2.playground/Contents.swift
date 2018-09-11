e85e1cff2521241dde140cb878d988fe8fda5a2f
Konstantin <ignatjev.konstantin@gmail.com>
2018-09-11 22:53:44 +0300

    Lesson 1 (61-62, 64, 72)

---
+++ Lesson 1/lesson2.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Задание
+
+//1 . Объявите три параметра . Первый из них должен быть константой типа Float с произвольным числовым значением, второй — пустой константой типа Float, третий — пустой переменной типа Double.
+
+let constFloat: Float = 1.23
+let constFloatTwo: Float
+var typeDouble: Double
+
+
+//2 . Установите новое произвольное значение всем параметрам, для которых эта операция возdможна.
+
+constFloatTwo = 124.4
+typeDouble = 256.2
---
+++ Lesson 1/lesson3.playground/Contents.swift
@@ -0,0 +1,40 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Задание 1
+
+// 1. Объявите три пустые константы: одну типа Int, одну типа Float и одну типа Double. Сделайте это в одной строке.
+
+let intConstNumber: Int, floatConstNumber: Float, doubleConstNumber: Double
+
+// 2 . Проинициализируйте для них следующие значения: Int — 18, Float — 16 .4, Double — 5 .7. Сделайте это в одной строке .
+
+intConstNumber = 18; floatConstNumber = 16.4; doubleConstNumber = 5.7
+
+// 3 . Найдите сумму всех трех констант и запишите ее в переменную типа Float.
+
+var sumCunt: Float = Float(intConstNumber) + Float(doubleConstNumber) + floatConstNumber
+
+// 4 . Найдите произведение всех трех констант и запишите его в переменную типа Int. Помните, что вам необходимо получить результат с минимальной погрешностью.
+
+var sumInt = Int(floatConstNumber * Float(doubleConstNumber) * Float(intConstNumber))
+
+// 5 . Найдите остаток от деления константы типа Float на константу типа Double и запишите ее в переменную типа Double.
+
+var sumDouble = Double(floatConstNumber) % doubleConstNumber
+
+//6 . Выведите на консоль все три результата с использованием поясняющего текста.
+
+
+
+// Задание 2
+
+//1 . Объявите переменные a и b целочисленного типа данных и присвойте им значения 2 и 3 .
+
+let a: Int = 2
+let b: Int = 3
+
+var sum = (a + 4)
+
+
